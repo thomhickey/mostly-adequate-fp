d7ab617b678d189a1ca1a362540cc9e7ebcaa62c
thom hickey <hickeyb@gene.com>
2020-01-15 17:24:19 -0800

    type hints

--- src/04/exercises/exercises_a.py
+++ src/04/exercises/exercises_a.py
@@ -1,7 +1,9 @@
+from typing import Callable
+
 # Refactor to remove all arguments by partially applying the function.
 
 # log :: int -> int
-log = lambda num: pow(10, num)
+log: Callable[[int], float] = lambda num: pow(10, num)
 
 # tests
 assert log(2) == 100
--- src/04/exercises/solution_a.py
+++ src/04/exercises/solution_a.py
@@ -1,8 +1,10 @@
+from typing import Callable
 # Refactor to remove all arguments by partially applying the function.
 
 # log :: int -> int
 from functools import partial
-log = partial(pow, 10)
+
+log: Callable[[], float] = partial(pow, 10)
 
 # tests
 assert log(2) == 100
