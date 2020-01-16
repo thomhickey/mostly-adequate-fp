ff4f938af0a578025ff10fead7081832f843c9cf
thom hickey <hickeyb@gene.com>
2020-01-15 17:08:38 -0800

    not liking

--- src/04/exercises/answers_b.py
+++ src/04/exercises/answers_b.py
@@ -3,8 +3,8 @@
 # js: const filterQs = xs => filter(x => x.match(/q/i), xs);
 import re
 from functools import partial
-filter_qs = partial(filter, partial(re.match, '.*[q].*'))
 
+filter_qs = partial(filter, partial(re.match, '.*[q].*'))  # dang, we can't return a list this way :(
 
 # tests
-assert list(filter_qs(['abc', 'qrs'])) == ['qrs']
+assert list(filter_qs(['abc', 'qrs'])) == ['qrs']  # dang filter objects!
